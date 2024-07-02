e28229a0a613a2a9c560bc6f0ab1337af229a4d4
Sansan338 <Negame0816@outlook.jp>
2024-07-02 19:39:00 +0900

    新しいゲームステータスを追加

--- Assets/MyGameAssets/Scripts/GameManager.cs
+++ Assets/MyGameAssets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public enum GameStatus { Play, GameOver, Result }     //ゲームステータス(プレイ状態、ゲームオーバー状態、リザルト状態)
+    public enum GameStatus { Play, GameOver, Result, Next}     //ゲームステータス(プレイ状態、ゲームオーバー状態、リザルト状態)
     public static GameStatus gameStatus;
 
     private int waitTime = 3;      //ゲームオーバー後にリザルト画面へ以降するまでの待機時間
